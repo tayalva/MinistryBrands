93ce7fef33ca1183901a0e392c28db16a5fd0404
Taylor Smith <taylor@taylorsmithcreates.com>
2019-11-19 21:38:03 -0800

    Initial Commit

---
+++ MinistryBrandsTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MinistryBrandsTestApp
+//
+//  Created by Taylor Smith on 11/19/19.
+//  Copyright © 2019 Taylor Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
